950b98cef9c8705e29373f3e7fe1117657b69cd8
Leandro <leandromcs96@gmail.com>
2020-02-01 12:22:14 -0300

    Autenticqa

--- app/src/main/java/com/example/whatsappclone/activity/AuthActivity.java
+++ app/src/main/java/com/example/whatsappclone/activity/AuthActivity.java
@@ -1,6 +1,10 @@
 package com.example.whatsappclone.activity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +18,14 @@ public class AuthActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        int auth = sharedPref.getInt(getString(R.string.phone_auth_preferences), 0);
+
+        if(auth == 1) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        }
+
         if (findViewById(R.id.fragment_container) != null) {
 
             if (savedInstanceState != null) {
@@ -22,11 +34,10 @@ public class AuthActivity extends AppCompatActivity {
 
             AuthFirstStepFragment authFirstStepFragment = new AuthFirstStepFragment();
 
-            //firstFragment.setArguments(getIntent().getExtras());
-
             getSupportFragmentManager().beginTransaction()
                                        .add(R.id.fragment_container, authFirstStepFragment)
                                        .commit();
         }
     }
+
 }
--- app/src/main/java/com/example/whatsappclone/fragment/AuthFirstStepFragment.java
+++ app/src/main/java/com/example/whatsappclone/fragment/AuthFirstStepFragment.java
@@ -1,9 +1,14 @@
 package com.example.whatsappclone.fragment;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
@@ -11,12 +16,31 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.whatsappclone.R;
+import com.example.whatsappclone.activity.HomeActivity;
 import com.example.whatsappclone.utils.MaskEditUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+import static android.content.ContentValues.TAG;
 
 public class AuthFirstStepFragment extends Fragment {
 
     private EditText codigo;
     private EditText telefone;
+    private Button btn_send;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private FirebaseAuth auth;
 
     @Nullable
     @Override
@@ -30,7 +54,84 @@ public class AuthFirstStepFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         this.codigo = getView().findViewById(R.id.et_codigo);
         this.telefone = getView().findViewById(R.id.et_tel);
-
+        this.btn_send = getView().findViewById(R.id.button);
+        this.auth = FirebaseAuth.getInstance();
         this.telefone.addTextChangedListener(MaskEditUtil.mask(telefone, MaskEditUtil.FORMAT_FONE));
+        this.btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSMS();
+            }
+        });
     }
+
+    private void sendSMS() {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(this.formatNumber(this.codigo.getText().toString(), this.telefone.getText().toString()), 60, TimeUnit.SECONDS, getActivity(),
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
+                        Log.d(TAG, "onVerificationCompleted:" + credential);
+
+                        signInWithPhoneAuthCredential(credential);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        Log.w(TAG, "onVerificationFailed", e);
+
+                        if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                            // Invalid request
+                            // ...
+                        } else if (e instanceof FirebaseTooManyRequestsException) {
+                            // The SMS quota for the project has been exceeded
+                            // ...
+                        }
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                        Log.d(TAG, "onCodeSent:" + verificationId);
+
+                        mVerificationId = verificationId;
+                        mResendToken = token;
+
+                        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, "123456");
+                        signInWithPhoneAuthCredential(credential);
+                    }
+                });
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithCredential:success");
+                            SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPref.edit();
+                            editor.putInt(getString(R.string.phone_auth_preferences), 1);
+                            editor.commit();
+
+                            Intent intent = new Intent(getActivity(), HomeActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                            }
+                        }
+                    }
+                });
+    }
+
+    private String formatNumber(String codigo, String telefone){
+        telefone = telefone.replace("(", "");
+        telefone = telefone.replace(")", "");
+        telefone = telefone.replace("-", "");
+        return "+" + codigo + telefone;
+    }
+
+
 }
